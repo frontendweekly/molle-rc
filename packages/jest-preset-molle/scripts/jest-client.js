70da56d0aeb472e56d7d988fd5e7384bbf5a43c6
Yuya Saito <hello@studiomohawk.com>
2020-12-27 00:24:26 +0900

    destruction was important...

--- packages/jest-preset-molle/scripts/jest-client.js
+++ packages/jest-preset-molle/scripts/jest-client.js
@@ -1,5 +1,5 @@
 module.exports = {
-  require('@frontendweekly/jest-preset-molle/scripts/jest-common.js'),
+  ...require('@frontendweekly/jest-preset-molle/scripts/jest-common.js'),
   displayName: 'client',
   testEnvironment: 'jest-environment-jsdom',
   testMatch: ['**/__client_tests__/**/*.js'],
--- packages/jest-preset-molle/scripts/jest-server.js
+++ packages/jest-preset-molle/scripts/jest-server.js
@@ -1,5 +1,5 @@
 module.exports = {
-  require('@frontendweekly/jest-preset-molle/scripts/jest-common.js'),
+  ...require('@frontendweekly/jest-preset-molle/scripts/jest-common.js'),
   displayName: 'server',
   testEnvironment: 'jest-environment-node',
   testMatch: ['**/__tests__/**/*.js'],
