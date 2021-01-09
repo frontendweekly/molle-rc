5022068d2094d7dc56216c983a116d5a97e2b42c
Yuya Saito <hello@studiomohawk.com>
2021-01-09 22:08:03 +0900

    fix: add spec

--- packages/jest-preset-molle/jest-preset.js
+++ packages/jest-preset-molle/jest-preset.js
@@ -16,13 +16,13 @@ module.exports = {
       displayName: 'client',
       setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
       testEnvironment: 'jest-environment-jsdom',
-      testMatch: ['**/__client_tests__/**/*.[jt]s?(x)'],
+      testMatch: ['**/__client_tests__/**/*.spec.[jt]s?(x)'],
     },
     {
       ...require('@frontendweekly/jest-preset-molle/jest-common.js'),
       displayName: 'server',
       testEnvironment: 'jest-environment-node',
-      testMatch: ['**/__tests__/**/*.[jt]s?(x)'],
+      testMatch: ['**/__tests__/**/*.spec.[jt]s?(x)'],
     },
   ],
 };
